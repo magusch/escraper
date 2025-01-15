429c0239be8f0ce7a9daaff84cb0d09491b3ac4d
magusch <magusch@gmail.com>
2025-01-15 12:52:33 +0700

    version 1.1.9.3: radario - refactor to json structure

--- escraper/parsers/radario.py
+++ escraper/parsers/radario.py
@@ -1,7 +1,6 @@
 import re
-import requests
 import warnings
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytz
 from bs4 import BeautifulSoup
@@ -10,41 +9,23 @@ from .base import BaseParser, ALL_EVENT_TAGS
 from ..emoji import add_emoji
 
 
-monthes = {
-    "января": "01",
-    "февраля": "02",
-    "марта": "03",
-    "апреля": "04",
-    "мая": "05",
-    "июня": "06",
-    "июля": "07",
-    "августа": "08",
-    "сентября": "09",
-    "октября": "10",
-    "ноября": "11",
-    "декабря": "12",
-}
-STRPTIME = "%d %m %H:%M"
-
-AVAILABLE_CATEGORIES = [
-    "concert",
-    "theatre",
-    "museum",
-    "education",
-    "sport",
-    "entertainment",
-    "kids",
-    "show",
-]
-
-
 class Radario(BaseParser):
     name = "radario"
-    BASE_URL = "https://spb.radario.ru/"
-    BASE_EVENTS_API = "https://radario.ru/events/"
+    BASE_URL = "https://radario.ru/events/"
+    BASE_EVENTS_API = "https://radario.ru/web-api/affiche/events"
     DATETIME_STRF = "%Y-%m-%d"
     parser_prefix = "RADARIO-"
 
+    AVAILABLE_CATEGORIES = [
+        "concert",
+        "theatre",
+        "museum",
+        "education",
+        "sport",
+        "entertainment",
+        "kids",
+        "show",
+    ]
 
     def __init__(self):
         self.url = self.BASE_URL
@@ -52,19 +33,23 @@ class Radario(BaseParser):
         self.timedelta_hours = self.timedelta_with_gmt0()
 
     def get_event(self, event_id=None, event_url=None, tags=None):
-        if event_id is not None and event_url is None:
-            event_url = "https://radario.ru/events/" + str(event_id)
-        elif event_url is None:
+        if event_id is None and event_url is not None:
+            event_id = event_url.split('/')[-1]
+        elif event_url is None and event_id is None :
             raise ValueError("'event_id' or 'event_url' required.")
 
-        event_soup = BeautifulSoup(
-            self._request_get(event_url).text, "lxml"
-        )
-        event = self.parse(event_soup, tags=tags or ALL_EVENT_TAGS)
+        event_api_url = f"{self.BASE_EVENTS_API}/{event_id}"
+        response = self._request_get(event_api_url)
+
+        if response.status_code != 200:
+            raise ValueError(f"Failed to fetch events. HTTP {response.status_code}: {response.text}")
+
+        event_json_data = response.json()
+        event = self.parse(event_json_data, tags=tags or ALL_EVENT_TAGS)
 
         return event
 
-    def get_events(self, request_params=None, tags=None):
+    def get_events(self, request_params=None, tags=None, existed_event_ids=[]):
         """
         Parameters:
         -----------
@@ -89,60 +74,77 @@ class Radario(BaseParser):
         Examples:
         ----------
         >>> radario = Radario()
-        >>> request_params = {
+        >>> request_params_general = {
             "online": True,
             "category": ["concert", "education", "theatre"],
             "date_from": "2020-01-01",
             "date_to": "2020-01-02",
         }
-        >>> radario.get_events(request_params=request_params)  # doctest: +SKIP
+        >>> radario.get_events(request_params_general=request_params)  # doctest: +SKIP
         """
         request_params = (request_params or dict())
 
         if "city" in request_params:
-            self.url = self.url.replace('spb', request_params['city'])
-            request_params.pop("city")
+            city_id = self.cities_to_id(request_params["city"])
+        else:
+            city_id = 1
+
 
         if request_params.pop("online", False):
-            # convert "https://spb.radario.ru/" to "https://online.radario.ru/"
-            self.url = self.url.replace("spb", "online")
+            is_online = True
+        else:
+            is_online = False
 
         events = list()
 
+        limit = 21
+        offset = 0
+
+        from_date = datetime.today() + timedelta(days=1)
+        if 'days' in request_params.keys():
+            to_date = from_date + timedelta(days=int(request_params["days"]))
+        else:
+            to_date = from_date + timedelta(days=8)
+
         for cat in request_params.pop("category", [""]):
-            if cat in AVAILABLE_CATEGORIES + [""]:
-                url = f"{self.url}{cat}"
-                response = self._request_get(url, params=request_params)
+            if cat in self.AVAILABLE_CATEGORIES + [""]:
+                while 100 >= offset:
+                    events_request_params = {
+                        "from": from_date.strftime("%Y-%m-%dT%H:%M:%S+03:00"),
+                        "to": to_date.strftime("%Y-%m-%dT%H:%M:%S+03:00"),
+                        "cityId": city_id,
+                        "limit": limit,
+                        "offset": offset,
+                        "online": is_online
+                    }
 
-                if response:
-                    # get only 20 events
-                    soup = BeautifulSoup(response.text, "lxml")
-                    list_event_from_soup = soup.find_all("div", {"class": "event-card"})
+                    if cat != "":
+                        events_request_params["superTagId"] = self.categories_to_id(cat)
 
-                else:
-                    list_event_from_soup = list()
+                    response = self._request_get(self.BASE_EVENTS_API, params=events_request_params)
 
-                for event_card in list_event_from_soup:
-                    event_id = event_card.find(
-                        "a", {"class": "event-card__title"}
-                    )["href"].split("/")[-1]
+                    if response:
+                        list_event_from_json = response.json()
+                    else:
+                        list_event_from_json = list()
+                    for event_json in list_event_from_json:
+                        event_id = self._id(event_json)
+                        if event_id in existed_event_ids: continue
 
-                    url = self.events_api + event_id
+                        new_event = self.get_event(event_id=event_json['id'], tags=tags or ALL_EVENT_TAGS)
 
-                    event_soup = BeautifulSoup(
-                        self._request_get(url).text, "lxml"
-                    )
-                    events.append(self.parse(event_soup, tags=tags or ALL_EVENT_TAGS))
+                        events.append(new_event)
+                        existed_event_ids.append(new_event.id)
+                    if len(list_event_from_json) < limit: break
+                    offset += (limit-1)
 
             else:
                 warnings.warn(f"Category {cat!r} is not exist", UserWarning)
 
         return events
 
-    def _adress(self, event_soup):
-        full_address = event_soup.find(
-            "span", {"class": "text-secondary mt-2"}
-        ).text.strip()
+    def _adress(self, event_json_data):
+        full_address = event_json_data['placeAddress'].strip()
 
         full_address = full_address.replace(", Центральный район", "")
 
@@ -154,200 +156,81 @@ class Radario(BaseParser):
         if "онлайн" in full_address.lower():
             address = "Онлайн"
         else:
-            if full_address.find(", Санкт-Петербург") != -1:
-                end_idx = full_address.find(", Санкт-Петербург")
+            city_name = event_json_data["cityName"]
+            if city_name is None: city_name = 'Санкт-Петербург'
+            if full_address.find(f", {city_name}") != -1:
+                end_idx = full_address.find(f", {city_name}")
                 address = full_address[:end_idx]
 
-            elif full_address.find("Санкт-Петербург, ") != -1:
-                address = full_address.replace("Санкт-Петербург, ", "")
-
+            elif full_address.find(f"{city_name}, ") != -1:
+                address = full_address.replace(f"{city_name}, ", "")
             else:
                 address = full_address
 
         return address
 
-    def _category(self, event_soup):
-        return event_soup.find("a", {"class": "event-page__tag"}).text.strip()
+    def _category(self, event_json_data):
+        return event_json_data['superTagName'].strip()
 
-    def _datetimes_from_html(self, event_soup):
-        """
-        Parse datetimes (from and to) from html page.
-        Use only in Radario._date_from()
-        """
-        strfdatetime = self._date_from_to(event_soup)
-
-        day_from = None
-        month_from = None
-        hour_from = None
-        minute_from = None
-
-        day_to = None
-        month_to = None
-        hour_to = None
-        minute_to = None
-
-        # can parse from-to datetime string formats:
-        # - "dd month, HH:MM-HH:MM"
-        # - "dd month, HH:MM"
-        # - "dd-dd month"
-        if not re.match(r"^\d\d \w+,", strfdatetime) \
-            and not re.match(r"^\d\d-\d\d \w+$", strfdatetime)\
-            and not re.match(r"^\d\d \w+ \d{4}", strfdatetime)\
-            and not re.match(r"^\d\d \w+-\d\d \w+", strfdatetime):
-            raise ValueError(
-                f"Unknown radario from-to datetime string: {strfdatetime!r}.\n"
-                f"Event url: {self._url(event_soup)}"
-            )
-
-        # dd month+
-        if re.match(r"^\d\d \w+,", strfdatetime):
-            day_from = int(strfdatetime[:2])
-            month_from = int(monthes[strfdatetime[3:].split(",")[0]])
-
-            day_to = day_from
-            month_to = month_from
-
-            # dd month, HH:MM
-            if re.match(r"\d{2} \w+, \d{2}:\d{2}$", strfdatetime):
-                strtime_from = strfdatetime[-5:]
-
-                hour_from = int(strtime_from.split(":")[0])
-                minute_from = int(strtime_from.split(":")[1])
-
-            # dd month, HH:MM-HH:MM
-            elif re.match(r"\d{2} \w+,.+\d{2}:\d{2}-\d{2}:\d{2}", strfdatetime):
-                strtime_from = strfdatetime[-11:-6]
-                hour_from = int(strtime_from.split(":")[0])
-                minute_from = int(strtime_from.split(":")[1])
-
-                strtime_to = strfdatetime[-5:]
-                hour_to = int(strtime_to.split(":")[0])
-                minute_to = int(strtime_to.split(":")[1])
-                if hour_to > self.timedelta_hours:
-                    hour_to = hour_to - self.timedelta_hours
-
-            if hour_from > self.timedelta_hours:
-                hour_from = hour_from - self.timedelta_hours
-
-        # dd-dd month
-        elif re.match(r"^\d\d-\d\d \w+$", strfdatetime):
-            day_from = int(strfdatetime[:2])
-            month_from = int(monthes[strfdatetime[6:]])
-            hour_from = 0
-            minute_from = 0
-
-            day_to = int(strfdatetime[3:5])
-            month_to = month_from
-            hour_to = 0
-            minute_to = 0
-        # dd month yyyy - #todo date_to and year
-        elif re.match(r"^\d\d \w+ \d{4}", strfdatetime):
-            strf_date_from , strf_date_to = strfdatetime.split(' - ')
-
-            day_from = int(strfdatetime[:2])
-            month_from = int(monthes[strfdatetime[3:].split(" ")[0]])
-            hour_from = 0
-            minute_from = 0
-
-            day_to = day_from
-            month_to = month_from
-        # dd month-dd month
-        elif re.match(r"^\d\d \w+-\d\d \w+", strfdatetime):
-            strf_date_from , strf_date_to = strfdatetime.split('-')
-            day_from, month_from = strf_date_from.split(' ')
-            day_from = int(day_from)
-            month_from = int(monthes[month_from.strip()])
-            day_to, month_to = strf_date_from.split(' ')
-            day_to = int(day_to)
-            month_to = int(monthes[month_to.strip()])
-            hour_from = 0
-            minute_from = 0
-
-        daytime_from = datetime.now().replace(
-            month=month_from,
-            day=day_from,
-            hour=hour_from,
-            minute=minute_from,
-            second=0,
-            microsecond=0,
-        ).astimezone(self.TIMEZONE)
-
-
-
-        if hour_to is not None and minute_to is not None:
-            daytime_to = datetime.now().replace(
-                month=month_to,
-                day=day_to,
-                hour=hour_to,
-                minute=minute_to,
-                second=0,
-                microsecond=0,
-            ).astimezone(self.TIMEZONE)
-        else:
-            daytime_to = None
-
-        return daytime_from, daytime_to
-
-    def _date_from(self, event_soup):
-        self._date_from_, self._date_to_ = self._datetimes_from_html(event_soup)
+    def _date_from(self, event_json_data):
+        self._date_from_ = datetime.strptime(event_json_data['beginDate'], '%Y-%m-%dT%H:%M:%S.%f%z')
         return self._date_from_
 
-    def _date_to(self, event_soup):
+    def _date_to(self, event_json_data):
+        self._date_to_ = datetime.strptime(event_json_data['endDate'], '%Y-%m-%dT%H:%M:%S.%f%z')
         return self._date_to_
 
-    def _date_from_to(self, event_soup):
+    def _date_from_to(self, event_json_data):
         """
         Parse date from and to as string from event page.
         """
-        return re.sub(
-            " +",
-            " ",
-            event_soup.find("span", {"class": "event-page__date mt-2"})
-            .text.strip()
-            .replace("\n", " ")
-        )
+        return f"{self._date_from_}-#{self._date_to_}"
 
-    def _id(self, event_soup):
-        meta_url = event_soup.find("meta", property="og:url")["content"]
-        return self.parser_prefix + meta_url[meta_url.rfind("/") + 1 :]
+    def _id(self, event_json_data):
+        return self.parser_prefix + str(event_json_data["id"])
 
-    def _place_name(self, event_soup):
-        return event_soup.find("span", {"class": "text-secondary mt-3"}).text.strip()
+    def _place_name(self, event_json_data):
+        return event_json_data["placeTitle"].strip()
 
-
-    def _full_text(self, event_soup) -> str:
-        return self.remove_html_tags(
-            event_soup.find("meta", property="og:description")["content"].replace(
-                "<br/>", "\n"
+    def _full_text(self, event_json_data) -> str:
+        if event_json_data["description"]:
+            return self.remove_html_tags(
+                event_json_data["description"].replace(
+                    "<br/>", "\n"
+                )
             )
-        )
+        else:
+            return ""
 
+    def _post_text(self, event_json_data):
+        return self.prepare_post_text(self._full_text(event_json_data))
 
-    def _post_text(self, event_soup):
-        return self.prepare_post_text(self._full_text(event_soup))
+    def _poster_imag(self, event_json_data):
+        event_image = event_json_data["imageUri"]
+        if event_image is not None:
+            return event_image
 
+    def _price(self, event_json_data):
+        return str(event_json_data["minPrice"]).split('.')[0] + event_json_data["currency"].replace('RUB', '₽')
 
+    def _title(self, event_json_data):
+        return add_emoji(event_json_data["title"].strip())
 
-    def _poster_imag(self, event_soup):
-        event_card_image = event_soup.find("img", {"class": "event-page__image"})
-        if (
-            event_card_image is not None
-            and "DefaultEventImage" not in event_card_image["src"]
-        ):
-            return event_card_image["src"]
+    def _url(self, event_json_data):
+        return self.BASE_URL + self._id(event_json_data).replace(self.parser_prefix, "")
 
-    def _price(self, event_soup):
-        return event_soup.find(
-            "button", {"class": "c-button c-button--primary c-button--medium"}
-        ).text.strip()
+    def _is_registration_open(self, event_json_data):
+        return event_json_data["ticketCount"] != 0
 
-    def _title(self, event_soup):
-        return add_emoji(
-            event_soup.find("h1", {"class": "event-page__title"}).text.strip()
-        )
+    def categories_to_id(self, category):
+        categories_to_id = {
+            "concert": 1, "theatre": 2,"museum": 3, "education": 4, "sport": 5,
+            "entertainment": 6, "kids": 380, "show": 1598, "Active": 1669,
+        }
+        return categories_to_id[category]
 
-    def _url(self, event_soup):
-        return self.events_api + self._id(event_soup).replace(self.parser_prefix, "")
-
-    def _is_registration_open(self, event_soup):
-        return self._price(event_soup) != "Билетов нет"
+    def cities_to_id(self, city):
+        cities_to_id = {
+            "spb": 1, "kzn": 85, "msk": 2,
+        }
+        return cities_to_id[city.lower()]
